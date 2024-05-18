acb4574a38d81be212480099334524bdc332ebb1
Nalin Kumar <nalin12894+github@gmail.com>
2024-05-19 00:51:10 +0530

    react hook form in schedule component

--- src/app/schedule-type.ts
+++ src/app/schedule-type.ts
@@ -1,9 +1,9 @@
 export interface Schedule {
-  created_by: number;
-  creation_date: Date | string;
-  last_updated_by: number;
-  last_update_date: Date | string;
-  id: number;
+  created_by?: number;
+  creation_date?: Date | string;
+  last_updated_by?: number;
+  last_update_date?: Date | string;
+  id?: number;
   schedule_id: string;
   cron_schedule: string;
 }
--- src/app/schedule/Schedule.tsx
+++ src/app/schedule/Schedule.tsx
@@ -1,7 +1,7 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { DataTable } from "../../components/ui/data-table-client/data-table";
 import { scheduleData } from "../schedule";
-import { EditableDeletableTable } from "./edit-dilog";
+import { EditDialogForm } from "./edit-dilog";
 import { Schedule } from "../schedule-type";
 import {
   columnList,
@@ -10,14 +10,18 @@ import {
   filterByCron,
 } from "./schedule-data";
 import { ColumnDefFun } from "@/components/ui/data-table-client/data-table-column-def";
-import { useCallback, useMemo } from "react";
+import { useCallback, useMemo, useState } from "react";
 
 const ScheduleComp = () => {
-  const data = scheduleData;
-
+  const [data, seteData] = useState(scheduleData);
+  const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
+  const [selectedRow, setSelectedRow] = useState<Schedule | null>(null);
   const onEdit = useCallback(
-    (data: Schedule) => alert(JSON.stringify(data)),
-    []
+    (data: Schedule) => {
+      setSelectedRow(data);
+      setIsEditDialogOpen(true);
+    },
+    [selectedRow]
   );
 
   const onDelete = useCallback(
@@ -47,7 +51,16 @@ const ScheduleComp = () => {
           filterCollDropdownOptions: filterByCron,
         }}
       />
-      <EditableDeletableTable tableData={data} />
+      <EditDialogForm
+        isEditDialogOpen={isEditDialogOpen}
+        onOpenDialogFunc={(value) => {
+          setIsEditDialogOpen(value);
+          if (!value) {
+            setSelectedRow(null);
+          }
+        }}
+        selectedRow={selectedRow}
+      />
     </div>
   );
 };
