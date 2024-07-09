88e1631e0fbac3c5e020baf44137011f2c13274f
Nalin Kumar <nalin12894+github@gmail.com>
2024-07-09 14:03:41 +0530

    removed unused scroll css

--- src/app/generalPages/index.tsx
+++ src/app/generalPages/index.tsx
@@ -14,7 +14,7 @@ export const GeneralPages = () => {
   return (
     <div className="">
       <FullPageHeader />
-      <div className="pt-20 md:pt-10  lg:pt-16 overflow-x-hidden overflow-y-scroll">
+      <div className="pt-20 md:pt-10  lg:pt-16">
         <Outlet />
       </div>
     </div>
